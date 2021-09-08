cbc79d7e84e8d50fda188986602644a097b45357
ChikaLong <longb.0818@gmail.com>
2021-09-08 08:16:27 +0000

    [Fix]エラー修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,10 +2,14 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @newbook=Book.new
+    @user=@book.user
   end
 
   def index
     @books = Book.all
+    @book=Book.new
+    @user=current_user
   end
 
   def create
@@ -15,12 +19,18 @@ class BooksController < ApplicationController
       redirect_to book_path(@book), notice: "You have created book successfully."
     else
       @books = Book.all
+      @user=current_user
       render 'index'
     end
   end
 
   def edit
     @book = Book.find(params[:id])
+    if @book.user == current_user
+      render :edit
+    else
+      redirect_to books_path
+    end
   end
 
 
@@ -34,16 +44,16 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title,:body)
   end
 
 end
